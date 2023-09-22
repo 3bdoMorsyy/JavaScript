4b7611a2543b577524e9fb083154d53ba4f1c585
3bdo <114808114+3bdoMorsyy@users.noreply.github.com>
2023-09-22 10:14:04 +0300

    Add files via upload

---
+++ Assignment/Introduction/master.js
@@ -0,0 +1,46 @@
+// Task 1
+
+/*Code One Won't Work Because Page Is not loaded and js file can't see the elemetns*/
+
+// Task 2
+/* 
+document.write("<h1>Elzero</h1>");
+document.querySelector("h1").style.color = "blue";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontFamily = "Ariall";
+*/
+
+// Task 3
+/*
+console.log(
+  "%cElzero %cWeb %cSchool",
+  "font-size:40px ; color:red;font-weight:bold",
+  "font-size:40px ; color:green;font-weight:bold",
+  "font-size:40px ; color:blue;font-weight:bold"
+);
+*/
+
+// Task 4
+/* 
+console.group("Group One");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child One");
+console.log("Message One");
+console.log("Message Two");
+console.group("Grand Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group Two");
+console.log("Message One");
+console.log("Message Two");
+*/
+
+// Task 5
+
+// console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
